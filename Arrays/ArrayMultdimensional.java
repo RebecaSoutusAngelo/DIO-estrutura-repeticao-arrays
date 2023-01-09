05c8c14c08e87145d0a03a43cefa2bf6543b2486
Rebeca Soutus Angelo <rebeca.soutusangelo@gmail.com>
2023-01-09 00:13:12 -0300

    c

---
+++ Arrays/ArrayMultdimensional.java
@@ -0,0 +1,24 @@
+package Arrays;
+
+public class ArrayMultdimensional {
+
+    /* Gere imprima uma matriz M 4x4 com valores aleatórios entre 0-9. */
+    public static void main (String[] args) {
+        Random random = new Random();
+
+        int[][] M = new int [4][4];
+
+        for(int i = 0; i < M.length; i++){
+            for( int j = 0; j < M[i].length; j++) {
+                M[i][j] = random.nextInt(bound: 9);
+            }
+        }
+        System.out.ptinln( "Matriz: ");
+        for (int[] linha : M ) {
+            for (int coluna : linha ) {
+                System.out.ptinln(coluna + " " );
+            }
+        }
+    }
+    
+}
